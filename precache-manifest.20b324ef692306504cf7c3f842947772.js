7b5b50aad0ae7918423b393e5b835be817240223
angeloDiepe <angelodiepe10@yahoo.com>
2019-11-02 06:47:18 +0100

    Updates

---
+++ precache-manifest.20b324ef692306504cf7c3f842947772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ac5de87347cc98c3a3a5cf01c89c5d",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "e988ab8c46d909067374",
+    "url": "/smartbrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8d43010237bbc66476a9",
+    "url": "/smartbrain/static/css/main.6aa4d16c.chunk.css"
+  },
+  {
+    "revision": "e988ab8c46d909067374",
+    "url": "/smartbrain/static/js/2.67326493.chunk.js"
+  },
+  {
+    "revision": "8d43010237bbc66476a9",
+    "url": "/smartbrain/static/js/main.dee94a49.chunk.js"
+  },
+  {
+    "revision": "7cc1afd37dac5b00c541",
+    "url": "/smartbrain/static/js/runtime-main.5a888777.js"
+  }
+]);
